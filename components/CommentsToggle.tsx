f34f83abc74658052caf1eae5bd03266fd41ed12
Shahzod <sibragimov999@gmail.com>
2024-06-19 15:06:50 +0500

    resize

--- components/CommentsToggle.tsx
+++ components/CommentsToggle.tsx
@@ -6,22 +6,29 @@ import Framer from "./magnetic/Magnetic";
 
 function CommentsToggle() {
   const [isVisible, setIsVisible] = useState(false);
-  const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth <= 900);
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth <= 900);
+    };
+
+    // Only run on the client side
+    if (typeof window !== "undefined") {
+      setIsSmallScreen(window.innerWidth <= 900);
+      window.addEventListener("resize", handleResize);
+    }
+
+    return () => {
+      if (typeof window !== "undefined") {
+        window.removeEventListener("resize", handleResize);
+      }
+    };
+  }, []);
 
   const toggleComments = () => {
     setIsVisible(!isVisible);
   };
-
-  const handleResize = () => {
-    setIsSmallScreen(window.innerWidth <= 900);
-  };
-
-  useEffect(() => {
-    window.addEventListener("resize", handleResize);
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
 
   return (
     <>
